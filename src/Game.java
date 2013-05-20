9c8079eb2e69953d9b0632ab89c5d45d7a0aa55c
Drilon Aziri <drilon.aziri@uni-duesseldorf.de>
2013-05-20 23:36:25 +0200

    StdDraw hinzugefügt und GameOver void

--- src/Game.java
+++ src/Game.java
@@ -4,5 +4,11 @@ public class Game {
 	public static void main(String[] args) throws IOException {
 		System.out.println(System.getProperty("user.dir") + "\\maps");
 		Maps.LoadMapList(3, System.getProperty("user.dir") + "\\maps");
+		
+		
+	}
+	
+	public static void GameOver() {
+		// TODO: GAMEOVER SCREEN
 	}
 }
