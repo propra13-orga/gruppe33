64c13fa2fb8449e174b22647d0de99a90f00f42c
Pierre-Henry Abraham <p.h.abraham@arcor.de>
2013-05-23 15:37:59 +0200

    Map Zeichnen funktioniert

--- src/DrawManager.java
+++ src/DrawManager.java
@@ -1,15 +1,24 @@
+import java.awt.Image;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class DrawManager {
-  public static void DrawMap(Map m) {
-    JFrame window = new JFrame();
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.setBounds(30, 30, 300, 300);
+	public static JFrame window;
+	public static void DrawMap(Map m) {
+		DrawManager.window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		int width = m.Size[0];
+		int height = m.Size[1];
+		
+		window.setBounds(30, 30, width * 32, height * 32);
+		
+		
+		MapCanvas mc = new MapCanvas();
+    	mc.MapToDraw = m;
     
-    MapCanvas mc = new MapCanvas();
-    mc.MapToDraw = m;
-    
-    window.getContentPane().add(mc);
-    window.setVisible(true);
-  }
+    	window.getContentPane().add(mc);
+    	window.setVisible(true);
+	}
 }
--- src/MapCanvas.java
+++ src/MapCanvas.java
@@ -9,9 +9,15 @@ public class MapCanvas extends JComponent {
 	
 	public void paint(Graphics g) {
 	    Graphics2D g2 = (Graphics2D) g;
-
-	    Image img1 = Toolkit.getDefaultToolkit().getImage("images/0.jpg");
-	    g2.drawImage(img1, 10, 10, this);
-	    g2.finalize();
+	    
+	    for(int y = 0; y <= this.MapToDraw.Size[1]; y++) {
+			for(int x = 0; x <= this.MapToDraw.Size[0]; x++) {
+				int zustand = this.MapToDraw.GetField(x, y);
+				
+				Image img1 = Toolkit.getDefaultToolkit().getImage("images/" + String.valueOf(zustand) + ".jpg");
+				g2.drawImage(img1, x * 32, y * 32, this);
+			    g2.finalize();
+			}
+		}
 	}
 }
