74ffb2a564f23b6a7c406de280e9c6b3f4c692ac
Drilon Aziri <drilon.aziri@uni-duesseldorf.de>
2013-05-28 01:15:29 +0200

    Kommentare hinzugefügt

--- src/Char.java
+++ src/Char.java
@@ -59,4 +59,5 @@ public class Char {
 			DrawManager.window.repaint();
 		}
 	}
+	
 }
--- src/Map.java
+++ src/Map.java
@@ -1,5 +1,10 @@
 import java.io.*;
 
+/**
+ * Map Objekt, welches alle Informationen für eine Map enthält
+ * @author Gruppe33
+ *
+ */
 public class Map {
 	public int Level;
 	public String Name;
@@ -11,10 +16,15 @@ public class Map {
 	public String Filename;
 	private String RawMapData;
 	
-	
+	/**
+	 * Konstruktor, initialisiert die Map und setzt die Felder entsprechend aus der Datei
+	 * @param filename Der Pfad der zu lesenden .map Datei
+	 * @throws IOException 
+	 */
 	public Map(String filename) throws IOException {
 		this.Filename = filename;
 		
+		// Datei wird eingelesen
 		FileReader freader = new FileReader(filename);
 		BufferedReader breader = new BufferedReader(freader);
 		
@@ -23,6 +33,7 @@ public class Map {
 		breader.close();
 		freader.close();
 		
+		// Paramter sind durch , getrennt.
 		String[] params = this.RawMapData.split(",");
 		this.Level = Integer.parseInt(params[0]);
 		this.Name = params[1];
@@ -30,10 +41,12 @@ public class Map {
 		this.Size = getSize;
 		
 		// Auch wenn der Programmierer bei 0 anfängt zu zählen, sollte das bei den MapDateien nicht so sein.
-		this.Fields = new int[this.Size[0] + 1][this.Size[1] + 1];
+		this.Fields = new int[this.Size[0]][this.Size[1]];
+		// Eingänge des Spielers Map
 		int[] playerstartpos = {Integer.parseInt(params[4]), Integer.parseInt(params[5])};
 		this.Player_Start_Positions = playerstartpos;
 		
+		// Ausgänge des Spielers auf der Map
 		int[] playerendpos = {Integer.parseInt(params[6]), Integer.parseInt(params[7])};
 		this.Player_End_Positions = playerendpos;
 		
@@ -48,7 +61,32 @@ public class Map {
 		}
 	}
 	
+	/**
+	 * Ruft den Zustand eines Feldes innerhalb der Map auf
+	 * @param x X-Koordinate des Feldes
+	 * @param y Y-Koordinate des Feldes
+	 * @return Gibt den Zustand des Feldes als Ganzzahl zurück 
+	 * (1: Begehbar, alles andere: Nicht begehbar)
+	 */
 	public int GetField(int x, int y) {
+		// Existiert Feld überhaupt, wenn nicht wird das Feld auch nicht aus dem Array geholt
+		if(!this.FieldExists(x, y)) {
+			return 0; 
+		}
+		
 		return this.Fields[x][y];
+	}
+	
+	/**
+	 * Prüft ob ein Feld ein gültiges Spielfeld ist
+	 * @param x X-Koordinate des Feldes
+	 * @param y Y-Koordinate des Feldes
+	 * @return Gibt true zurück, wenn sich das Feld x,y auf Map m befindet.
+	 */
+	private Boolean FieldExists(int x, int y) {
+		int width = this.Size[0] * 32;
+		int height = this.Size[1] * 32;
+		
+		return x < 0 || y < 0 || x > width || y > height;
 	}
 }
